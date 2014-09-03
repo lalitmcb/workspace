4cfbb6aeaffa41458e15178ff76513866f480905
lalit.bhatt@gmail.com <lalit.bhatt@gmail.com>
2014-09-03 11:25:19 +0530

    formating

--- activeMQ/src/main/java/com/lalit/example/activemq/ActiveMQMain.java
+++ activeMQ/src/main/java/com/lalit/example/activemq/ActiveMQMain.java
@@ -1,30 +1,35 @@
 package com.lalit.example.activemq;
 
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class ActiveMQMain {
-	
-	public static void main(String[] args){
-//Create the connection factory
-ActiveMQConnectionFactory connectionFactory = 
-		new ActiveMQConnectionFactory("tcp://localhost:61616");
 
-//Create the consumer. It will wait to listen to the Topic
-Thread topicConsumerThread = 
-		new Thread(new TopicConsumer(connectionFactory));
-topicConsumerThread.start();
+	public static void main(String[] args) throws JMSException {
+		// Create the connection factory
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+				"tcp://localhost:61616");
 
-try {
-	Thread.sleep(1000);
-} catch (InterruptedException e) {
-	e.printStackTrace();
-}
+		// Create the consumer. It will wait to listen to the Topic
+		Thread topicConsumerThread = new Thread(new TopicConsumer(
+				connectionFactory));
+		topicConsumerThread.start();
 
-//Create a message. As soon as the message is published on the Topic,
-//it will be consumed by the consumer
-Thread topicProducerThread = 
-		new Thread(new TopicProducer(connectionFactory));
-topicProducerThread.start();	
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Create a message. As soon as the message is published on the Topic,
+		// it will be consumed by the consumer
+		Thread topicProducerThread = new Thread(new TopicProducer(
+				connectionFactory));
+		topicProducerThread.start();
 		
 	}
 
